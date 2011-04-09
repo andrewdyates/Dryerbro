8f7205f17097d9d095e99e2010e106551867fdca
Eric Kerr <EricPKerr@gmail.com>
2011-04-08 20:36:57 -0400

    Lowering threshold

--- Resources/js/statemachine.js
+++ Resources/js/statemachine.js
@@ -33,7 +33,7 @@ var StateMachine = {
   
   RUNNING_MAX_RESET_TRIES: 40, // Number of times to reset in running before throwing an error
   
-  ACTIVE_VIBRATION_THRESHOLD: 9,
+  ACTIVE_VIBRATION_THRESHOLD: 3,
   
   _callbackHandler: null, // Pointer to handler of current vibration callbacks
   
